3e6e8ec46a0a8e6d58fcf0ad377e9eeefe88ba7f
cjerez-i061n <c.jeroch2904@gmail.com>
2025-09-23 21:47:58 -0600

    Funcionalidad de Login

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,38 @@ import { LoginForm } from './components/auth/LoginForm';
 import Swal from 'sweetalert2';
 
 function App() {
-  
+
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   const handleDrawerToggle = () => setDrawerOpen(!drawerOpen);
 
   const handleLogout = () => {
-    Swal.fire 
-    window.location.href='/login'
+    Swal.fire({
+      title: "Logout",
+      text: "Está seguro de cerrar sesión?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = '/login'
+      }
+    });
   }
 
   return (
     <>
-    <Router>
-      <CssBaseline/>
-      <AppBarMenu onMenuClick={handleDrawerToggle} onLogout={handleLogout}></AppBarMenu>
-      <SideNav open={drawerOpen} onClose={handleDrawerToggle}></SideNav>
-      <Routes>
-          <Route path='/login' element={<LoginForm onLoginSuccess={() => window.location.href='www.google.com.gt'}/>}/>
-      </Routes>
-    </Router>
-      
+      <Router>
+        <CssBaseline />
+        <AppBarMenu onMenuClick={handleDrawerToggle} onLogout={handleLogout}></AppBarMenu>
+        <SideNav open={drawerOpen} onClose={handleDrawerToggle}></SideNav>
+        <Routes>
+          <Route path='/login' element={<LoginForm onLoginSuccess={() => window.location.href = 'www.google.com.gt'} />} />
+        </Routes>
+      </Router>
+
     </>
   )
 }
