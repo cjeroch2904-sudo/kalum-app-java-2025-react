8e20acad9e1b1529a8df3074c7f0db9437ae118c
cjerez-i061n <c.jeroch2904@gmail.com>
2025-09-27 16:37:25 -0600

    Actualización app

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import './App.css'
 import { AppBarMenu } from './components/layout/AppBarMenu'
 import { useState } from 'react';
 import { SideNav } from './components/layout/SideNav';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { LoginForm } from './components/auth/LoginForm';
 import Swal from 'sweetalert2';
+import { CareerList } from './components/careers/CareerList';
 
 function App() {
 
@@ -36,7 +37,9 @@ function App() {
         <AppBarMenu onMenuClick={handleDrawerToggle} onLogout={handleLogout}></AppBarMenu>
         <SideNav open={drawerOpen} onClose={handleDrawerToggle}></SideNav>
         <Routes>
-          <Route path='/login' element={<LoginForm onLoginSuccess={() => window.location.href = 'www.google.com.gt'} />} />
+          <Route path='/login' element={<LoginForm onLoginSuccess={() => window.location.href = '/careers'} />} />
+          <Route path='/careers' element={<CareerList/>} />  
+          <Route path='/' element={<Navigate to="/careers"/>}/>
         </Routes>
       </Router>
 
